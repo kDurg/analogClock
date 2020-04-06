02ba81ac752d2fb6086dc2e4306d85d5233b18da
Kyle Durigan <kyle.durigan@gmail.com>
2020-04-06 10:36:51 -0600

    fix degree rotation for hour/ minute hands

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-// Planet Images Source: https://www.freepik.com/free-vector/milky-way-planets_800857.htm
+// Planet Images Source: https://www.freepik.com/free-vector/milky-way-planets_800857.html
 // Color Palette: https://colorswall.com/palette/134/
 
 setInterval(function () {
+    let i=0;
     const hourHand = document.querySelector('.hour-hand');
     const minuteHand = document.querySelector('.minute-hand');
     const secondHand = document.querySelector('.second-hand');
@@ -12,16 +13,23 @@ setInterval(function () {
         now.getDate(),
         0, 0, 0)
     const mil = now.getTime() - midnight.getTime(); // GET MILLISECOND OFFSET FOR SMOOTHER SECOND HAND MOVEMENT
-    const h = (mil / (1000 * 60 * 60));
+    const h = (mil / 1000 / 60 / 60);
     const m = (h * 60);
     const s = (m * 60);
-    
-    const hourDegree = h + (h / 2);
+
+    const hourDegree = h * 30 + (h / 2) + 90;
     hourHand.style.transform = `rotate(${hourDegree}deg)`;
     
-    const minuteDegree = m * 3;
+    const minuteDegree = m * 6 + 90;
     minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
     
     const secondDegree = (s * 6);
     secondHand.style.transform = `rotate(${secondDegree}deg)`;
+    console.log('THIS: ', hourDegree)
+
+    if(i>0){
+        document.querySelector("#clock").addClass("transform");       
+    }
+    i++;
+
 }, 1000);
